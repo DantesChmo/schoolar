d98a98749bf76c1d2db12d9352ef7a5202d969b6
DantesChmo <doni-rio@yandex.ru>
2021-05-23 03:20:38 +0300

    test

--- infra/actions/test/index.js
+++ infra/actions/test/index.js
@@ -1,12 +1,17 @@
 const { context, getOctokit } = require('@actions/github/lib/github');
 console.log('Hello I am action');
 
-const client = getOctokit('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');
 
 const { payload } = context;
 const pullNumber = payload.pull_request.number;
 const owner = payload.repository.owner.login;
 const repo = payload.repository.name;
+
+console.log(pullNumber);
+console.log(owner);
+console.log(repo);
+
+const client = getOctokit('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');
 
 (async () => {
   const result = await client.rest.pulls.listFiles({
