d8c87fe18f5c7e1d2063c8d39189d68f192b64c8
DantesChmo <doni-rio@yandex.ru>
2021-05-23 04:18:59 +0300

    one more thing

--- infra/actions/test/index.js
+++ infra/actions/test/index.js
@@ -7,16 +7,10 @@ const pullNumber = payload.pull_request.number;
 const owner = payload.repository.owner.login;
 const repo = payload.repository.name;
 
-console.log(process.env.GITHUB_REF);
-console.log(process.env.GITHUB_EVENT_PATH);
-console.log(payload.pull_request);
-console.log(pullNumber);
-console.log(owner);
-console.log(repo);
-
 (async () => {
   const result = await get(`https://api.github.com/repos/${owner}/${repo}/pulls/${pullNumber}/files`);
-  console.log(result);
+  const files = JSON.stringify(result.body).map(({fileName}) => fileName);
+  console.log(files);
 
   process.exit(0);
 })();
